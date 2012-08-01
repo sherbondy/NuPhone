b2fe20cce0844d81d447ef2a4409ead8d2becce1
Ethan Sherbondy <sherbondye@gmail.com>
2012-08-01 13:23:16 -0700

    Initial Commit

---
+++ NuPhone/NPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  NPAppDelegate.h
+//  NuPhone
+//
+//  Created by Ethan Sherbondy on 8/1/12.
+//  Copyright (c) 2012 Ethan Sherbondy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ NuPhone/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  NuPhone
+//
+//  Created by Ethan Sherbondy on 8/1/12.
+//  Copyright (c) 2012 Ethan Sherbondy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "NPAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([NPAppDelegate class]));
+    }
+}
